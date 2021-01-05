61978020b56e3828a4520e0a0c03aa76c287d0e4
Rob Patro <rob@newton>
2021-01-04 22:46:59 -0500

    variable for program name

--- quant_kallisto.sh
+++ quant_kallisto.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+KALLISTO=kallisto
+
 ##
 # unstranded (as in Varabyou et al.)
 ##
-parallel -j 5 kallisto quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_all_default --single simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${KALLISTO} quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_all_default --single simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
-parallel -j 5 kallisto quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_real_default --single simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${KALLISTO} quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_real_default --single simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
 ##
 # strand-specific 
 ##
-parallel -j 5 kallisto quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_all_stranded_default --fr-stranded --single simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${KALLISTO} quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_all_stranded_default --fr-stranded --single simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
-parallel -j 5 kallisto quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_real_stranded_default --fr-stranded --single simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${KALLISTO} quant -i ref/kallisto_idx -l 101 -s 0.0001 -t 8 -o quants/tissue{1}/sample{2}/kallisto_real_stranded_default --fr-stranded --single simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
--- quant_sal.sh
+++ quant_sal.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+SAL=salmon
+
 ##
 # full index
 ##
 
 # default
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_all_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_all_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_real_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_real_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
 # em 
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_all_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_all_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_real_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx_saf -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_real_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
 
 ##
@@ -20,12 +22,12 @@ parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx_saf -lA -r simul
 ##
 
 # default
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_all_nodecoy_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_all_nodecoy_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_real_nodecoy_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 -o quants/tissue{1}/sample{2}/salmon_real_nodecoy_default ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
 # em 
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_all_nodecoy_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_all_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_all_nodecoy_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
-parallel -j 5 ~/salmon/build/src/salmon quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_real_nodecoy_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
+parallel -j 5 ${SAL} quant -i ref/salmon_idx -lA -r simulated_reads/tissue{1}/sample{2}/simulated_real_shuffled.fq.gz -p 8 --fldMean 101 --fldSD 0.5 --useEM -o quants/tissue{1}/sample{2}/salmon_real_nodecoy_em ::: 0 1 2 ::: 0 1 2 3 4 5 6 7 8 9
 
